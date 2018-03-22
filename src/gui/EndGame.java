ac2bf370c797455bf5ba5ca9e0d3ecbc1eb052fe
TheDustyMan <popescu_andrei27@yahoo.com>
2018-03-22 10:57:29 +0200

    Got rid of useless stuff in EndGame class

--- src/gui/EndGame.java
+++ src/gui/EndGame.java
@@ -14,41 +14,6 @@ import logic.Boy;
 
 public class EndGame extends JPanel {
 	
-	public EndGame(){
-		this.setSize(GameFrame.WIDTH, GameFrame.HEIGHT);
-		this.setLayout(null);
-		loadInformations();
+	
 	}
 
-	private void loadInformations() {
-		try {
-			
-			gameOver=ImageIO.read(getClass().getResource("../images/gameOver.png"));
-			pixelFont=Font.createFont(Font.TRUETYPE_FONT, getClass().getResourceAsStream("../fonts/pixel.ttf")).deriveFont(35.0f);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (FontFormatException e) {
-			e.printStackTrace();
-		}
-	}
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Graphics2D g2=(Graphics2D)g;
-		g2.setColor(Color.WHITE);
-		g2.setFont(pixelFont);
-		
-		if(boy.getLife()<=0){
-			g2.drawImage(gameOver,0,0,GameFrame.WIDTH,GameFrame.HEIGHT,null);
-		}
-	}
-	
-
-	public void addBoy(Boy boy) {
-		this.boy=boy;
-	}
-	
-	private BufferedImage gameOver;
-	private Boy boy;
-	private Font pixelFont;
-}
